bebb2c8a586323b31538789e4d2ddbad201f38ad
Crowdbotics <team@crowdbotics.com>
2021-04-13 21:23:13 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/expiry_date_timer_25671/MainActivity.java
@@ -0,0 +1,15 @@
+package com.expiry_date_timer_25671;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "expiry_date_timer_25671";
+  }
+}
